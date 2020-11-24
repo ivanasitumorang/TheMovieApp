5ecab6b6c8753de62b1996255bdf36ced6dc059c
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-11-25 00:16:24 +0700

    feat(tv-series): add tv series adapter and populate the data

--- app/src/main/java/com/azuka/themovieapp/feature/tvseries/TvSeriesFragment.kt
+++ app/src/main/java/com/azuka/themovieapp/feature/tvseries/TvSeriesFragment.kt
@@ -1,20 +1,37 @@
 package com.azuka.themovieapp.feature.tvseries
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.viewModels
 import com.azuka.themovieapp.BaseFragment
 import com.azuka.themovieapp.R
+import kotlinx.android.synthetic.main.fragment_tv_series.*
 
 
 /**
  * Created by ivanaazuka on 24/11/20.
  * Android Engineer
  */
- 
+
 class TvSeriesFragment : BaseFragment() {
+
+    private val viewModel: TvSeriesViewModel by viewModels()
+
     override val viewLayout: Int = R.layout.fragment_tv_series
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
-
+        setupUI()
     }
 
+    private fun setupUI() {
+        val tvSeriesList = viewModel.getTvSeriesDummy()
+        val adapter = TvSeriesAdapter(tvSeriesList) { tvSeries ->
+            showToast(tvSeries.name)
+        }
+        rvTvSeries.adapter = adapter
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
 }
---
+++ app/src/main/java/com/azuka/themovieapp/feature/tvseries/TvSeriesViewModel.kt
@@ -0,0 +1,19 @@
+package com.azuka.themovieapp.feature.tvseries
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.azuka.themovieapp.data.TvSeries
+import com.azuka.themovieapp.utils.DummyData
+
+
+/**
+ * Created by ivanaazuka on 25/11/20.
+ * Android Engineer
+ */
+
+class TvSeriesViewModel(private val app: Application) : AndroidViewModel(app) {
+
+    fun getTvSeriesDummy(): List<TvSeries> {
+        return DummyData.getDummyTvSeries(app.applicationContext).results
+    }
+}
