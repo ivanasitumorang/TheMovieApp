fed339b5a8adc19cb1768c66ba3c8b6236731cae
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-11-24 19:49:19 +0700

    feat(converter-extension): add json converter extension

---
+++ app/src/main/java/com/azuka/themovieapp/extension/ConverterExtension.kt
@@ -0,0 +1,28 @@
+package com.azuka.themovieapp.extension
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+
+/**
+ * Created by ivanaazuka on 24/11/20.
+ * Android Engineer
+ */
+
+val gson = Gson()
+
+//convert a data class to a map
+fun <DC> DC.serializeToMap(): Map<String, Any> {
+    return convert()
+}
+
+//convert an object of type I to type O
+inline fun <I, reified O> I.convert(): O {
+    val json = gson.toJson(this)
+    return gson.fromJson(json, object : TypeToken<O>() {}.type)
+}
+
+//convert a string json to a map
+inline fun <reified M> String.convertStringToMap(): M {
+    return gson.fromJson(this, object : TypeToken<M>() {}.type)
+}
