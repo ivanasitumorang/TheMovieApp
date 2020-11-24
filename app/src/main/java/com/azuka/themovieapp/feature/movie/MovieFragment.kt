7fa5a120a2f731f8ef5bc6560f5b699e3743d12b
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-11-24 21:45:54 +0700

    feat(movie): add function get dummy data movie

--- app/src/main/java/com/azuka/themovieapp/feature/movie/MovieFragment.kt
+++ app/src/main/java/com/azuka/themovieapp/feature/movie/MovieFragment.kt
@@ -1,6 +1,7 @@
 package com.azuka.themovieapp.feature.movie
 
 import android.os.Bundle
+import androidx.fragment.app.viewModels
 import com.azuka.themovieapp.BaseFragment
 import com.azuka.themovieapp.R
 
@@ -9,11 +10,14 @@ import com.azuka.themovieapp.R
  * Created by ivanaazuka on 24/11/20.
  * Android Engineer
  */
- 
+
 class MovieFragment : BaseFragment() {
+
+    private val viewModel: MovieViewModel by viewModels()
+
     override val viewLayout: Int = R.layout.fragment_movie
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
-
+        val result = viewModel.getMoviesDummy()
     }
 }
---
+++ app/src/main/java/com/azuka/themovieapp/feature/movie/MovieViewModel.kt
@@ -0,0 +1,19 @@
+package com.azuka.themovieapp.feature.movie
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.azuka.themovieapp.data.Movie
+import com.azuka.themovieapp.utils.DummyData
+
+
+/**
+ * Created by ivanaazuka on 24/11/20.
+ * Android Engineer
+ */
+
+class MovieViewModel(private val app: Application) : AndroidViewModel(app) {
+
+    fun getMoviesDummy(): List<Movie> {
+        return DummyData.getDummyMovies(app.applicationContext).results
+    }
+}
