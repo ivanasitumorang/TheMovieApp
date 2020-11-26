87aee417f18ca555486fabde5298f361e041865a
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-11-26 14:12:47 +0700

    refactor(home): delete unused code

--- app/src/main/java/com/azuka/themovieapp/feature/HomeFragment.kt
+++ app/src/main/java/com/azuka/themovieapp/feature/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import com.azuka.themovieapp.BaseFragment
 import com.azuka.themovieapp.R
 import com.azuka.themovieapp.adapter.TabAdapter
-import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_home.*
 
 
@@ -12,7 +11,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
  * Created by ivanaazuka on 24/11/20.
  * Android Engineer
  */
- 
+
 class HomeFragment : BaseFragment() {
     override val viewLayout: Int = R.layout.fragment_home
 
@@ -30,20 +29,6 @@ class HomeFragment : BaseFragment() {
         val adapter = TabAdapter(childFragmentManager)
         pager.adapter = adapter
         tabLayout.setupWithViewPager(pager)
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                pager.currentItem = tab?.position ?: 0
-            }
-
-        })
     }
 
 }
