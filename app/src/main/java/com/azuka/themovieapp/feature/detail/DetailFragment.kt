008faf7a0607210f6933b836ac846434d687169d
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-11-27 01:11:57 +0700

    feat(detail): add detail page

--- app/src/main/java/com/azuka/themovieapp/feature/detail/DetailFragment.kt
+++ app/src/main/java/com/azuka/themovieapp/feature/detail/DetailFragment.kt
@@ -1,20 +1,27 @@
 package com.azuka.themovieapp.feature.detail
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.azuka.themovieapp.BaseFragment
+import com.azuka.themovieapp.BuildConfig
 import com.azuka.themovieapp.R
 import com.azuka.themovieapp.data.Movie
 import com.azuka.themovieapp.data.TvSeries
 import com.azuka.themovieapp.utils.Constants
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 
 /**
  * Created by ivanaazuka on 24/11/20.
  * Android Engineer
  */
- 
+
 class DetailFragment : BaseFragment() {
 
     private var dataType: String? = null
@@ -23,6 +30,13 @@ class DetailFragment : BaseFragment() {
     private val viewModel: DetailViewModel by viewModels()
 
     override val viewLayout: Int = R.layout.fragment_detail
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbarDetail.setupWithNavController(navController, appBarConfiguration)
+    }
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
         arguments?.let { args ->
@@ -48,6 +62,16 @@ class DetailFragment : BaseFragment() {
     private fun setupContentMovie(movie: Movie?) {
         movie?.let {
             showToast(movie.title)
+            val moviePosterPath = "${BuildConfig.TMDB_IMAGE_URL}/w500/${movie.posterPath}"
+            Picasso.get().load(moviePosterPath).into(ivDetailImage)
+            tvDetailTitle.text = movie.title
+            rbDetail.rating = (movie.voteAverage / 2).toFloat()
+            tvDetailRating.text = ("(${movie.voteAverage} of 10)")
+            tvDetailReleaseDate.text = movie.releaseDate
+            tvDetailLanguage.text = movie.originalLanguage
+            tvDetailVoteCount.text = movie.voteCount.toString()
+            tvDetailOverview.text = movie.overview
+
         }
     }
 
