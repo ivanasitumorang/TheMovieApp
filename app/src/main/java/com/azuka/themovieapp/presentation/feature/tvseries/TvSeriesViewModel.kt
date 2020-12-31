2e7f972554396eea1d421147c5850bc5841b2c7c
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-12-31 12:25:48 +0700

    refactor(tv-series-view-model): delete unused codes

--- app/src/main/java/com/azuka/themovieapp/presentation/feature/tvseries/TvSeriesViewModel.kt
+++ app/src/main/java/com/azuka/themovieapp/presentation/feature/tvseries/TvSeriesViewModel.kt
@@ -2,7 +2,6 @@ package com.azuka.themovieapp.presentation.feature.tvseries
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.azuka.themovieapp.data.source.Repository
 import com.azuka.themovieapp.presentation.entity.TvSeries
@@ -15,9 +14,6 @@ import com.azuka.themovieapp.presentation.entity.TvSeries
 
 class TvSeriesViewModel @ViewModelInject constructor(private val repository: Repository) :
     ViewModel() {
-
-    private val _tvSeriesList = MediatorLiveData<List<TvSeries>>()
-    val tvSeriesList: LiveData<List<TvSeries>> = _tvSeriesList
 
     fun getTvSeries(): LiveData<List<TvSeries>> = repository.getTvSeries()
 
