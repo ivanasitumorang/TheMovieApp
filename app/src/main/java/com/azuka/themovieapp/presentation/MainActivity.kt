d360bc820f9968408eba5b6f555cc9c01240093b
ivanaazuka <ivana.situmorang@amarbank.co.id>
2021-01-09 19:20:41 +0700

    feat(main-activity): add bottom navigation view

--- app/src/main/java/com/azuka/themovieapp/presentation/MainActivity.kt
+++ app/src/main/java/com/azuka/themovieapp/presentation/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.azuka.themovieapp.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.azuka.themovieapp.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -25,6 +28,14 @@ class MainActivity : AppCompatActivity() {
 
         navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navGraph = navController.graph)
+        bottomNavView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.favoritesFragment -> bottomNavView.visibility = View.VISIBLE
+                else -> bottomNavView.visibility = View.GONE
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
